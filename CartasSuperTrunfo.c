d633a77e449673a6acd17b9e682bbbf30a458988
LucasJonas <lucas.jonas720@gmail.com>
2025-05-09 21:20:56 +0000

    1.0

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,16 +7,71 @@
 //Teste larissa
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    char estado, codigoDaCarta[4], NomeCidade[10];
+
+    int populacao, PontosTuristicos;
+
+    float areaKm, pib;
+
+    printf("Insira uma letra de A a H para representar um dos oito estados\n");
+    scanf("%c",&estado); // precisa do &
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    printf("Insira a lera do estado seguido de um numero de 01 ate 04\n");
+    scanf("%s",&codigoDaCarta); // teste ok
     
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Escolha o nome da cidade\n");
+    scanf("%s",&NomeCidade);
+
+    printf("Insira o numero de habitantes da cidade\n");
+    scanf("%d",&populacao);
+
+    printf("Insira a quantidade de pontos turisticos\n");
+    scanf("%d",&PontosTuristicos);
+
+    printf("Insira area da cidade em km\n");
+    scanf("%f",&areaKm);
+
+    printf("Insira o valor do PIB\n");
+    scanf("%f",&pib);
+
+    pib = pib /100; // coverter para bilhao
+
+    printf("**CARTA 1\nEstado:   %c\ncodigo Da Carta:   %s\nNomeCidade:   %s\n", estado,codigoDaCarta,NomeCidade); //char
+    printf("Numero De Habitantes:   %d\nPontos Turisticos:   %d\n", populacao, PontosTuristicos);// int
+    printf("Area em km:   %.2f\nPIB:   %7.2f bilhoes de reais\n**",areaKm,pib);
+
+    char estado2, codigoDaCarta2[4], NomeCidade2[10];
+
+    int populacao2, PontosTuristicos2;
+
+    float areaKm2, pib2;
+
+    printf("Insira uma letra de A a H para representar um dos oito estados\n");
+    scanf(" %c",&estado2); // precisa do &
+    
+    printf("Insira a lera do estado seguido de um numero de 01 ate 04\n");
+    scanf("%s",&codigoDaCarta2); // teste ok
+    
+    printf("Escolha o nome da cidade\n");
+    scanf("%s",&NomeCidade2);
+
+    printf("Insira o numero de habitantes da cidade\n");
+    scanf("%d",&populacao2);
+
+    printf("Insira a quantidade de pontos turisticos\n");
+    scanf("%d",&PontosTuristicos2);
+
+    printf("Insira area da cidade em km\n");
+    scanf("%f",&areaKm2);
+
+    printf("Insira o valor do PIB\n");
+    scanf("%f",&pib2);
+
+    pib2 = pib2 /100; // coverter para bilhao
+
+    printf("**CARTA 2\nEstado:   %c\ncodigo Da Carta:   %s\nNomeCidade:   %s\n", estado2,codigoDaCarta2,NomeCidade2); //char
+    printf("Numero De Habitantes:   %d\nPontos Turisticos:   %d\n", populacao2, PontosTuristicos2);// int
+    printf("Area em km:   %.2f\nPIB:   %7.2f bilhoes de reais\n**",areaKm2,pib2);
 
     return 0;
 }
